fe41afdcf747054e4517e6309525885f24b05edd
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-06-11 20:07:36 +0200

    treewide: prepare for One UI 7

--- customize.sh
+++ customize.sh
@@ -3,6 +3,6 @@ mkdir -p "$WORK_DIR/vendor/tee"
 SET_METADATA "vendor" "tee" 0 2000 755 "u:object_r:tee_file:s0"
 
 if ! grep -q "tee_file (dir (mounton" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
-    echo "(allow init_31_0 tee_file (dir (mounton)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-    echo "(allow priv_app_31_0 tee_file (dir (getattr)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    echo "(allow init_202404 tee_file (dir (mounton)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    echo "(allow priv_app_202404 tee_file (dir (getattr)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
 fi
