190df9df15ca91106de493d4bbb1227fc953d808
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-08-05 21:24:08 +0200

    chore: prepare for public release
    * Update prebuilts to UN1CA@c2fcd66d
    * Make project GPLv3*
    * Switch to samloader
    * Update README
    * Add issue template to request One UI 8+ bootloaders
    * Add a list of supported bootloaders to vendor build.prop
    
    *This is done to encourage development for Galaxy A34.
    This excludes prebuilts from Samsung in the tee_eea, tee_sea, tee_kor, tee_chn, and tee_latam directories.
    Samsung is the exclusive owner of all the contents in those folders.

---
+++ .github/ISSUE_TEMPLATE/one-ui-8--bootloader-version-request.md
@@ -0,0 +1,17 @@
+---
+name: One UI 8+ Bootloader version request
+about: Request support for a One UI 8+ bootloader version
+title: "[BL VERSION] Add support for firmware"
+labels: bootloader-version
+assignees: Fede2782
+
+---
+
+**Your CSC**
+[ Insert here ]
+
+**Your firmware version**
+[ Insert here ]
+
+**Your model**
+[ Insert here ]
--- customize.sh
+++ customize.sh
@@ -6,3 +6,5 @@ if ! grep -q "tee_file (dir (mounton" "$WORK_DIR/vendor/etc/selinux/vendor_sepol
     echo "(allow init_202404 tee_file (dir (mounton)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
     echo "(allow priv_app_202404 tee_file (dir (getattr)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
 fi
+
+SET_PROP "vendor" "ro.vendor.teegris.supported_bootloaders" "$(cat "$(dirname ${BASH_SOURCE[0]})/supported_bootloaders" | tr '\n' ',' | sed 's/,$//')"
