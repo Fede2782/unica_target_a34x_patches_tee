c28b21c7ea3cd40fb055bd1f44677058829a6ff1
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-08-12 09:06:08 +0200

    customize: use all property with multitee

--- customize.sh
+++ customize.sh
@@ -7,7 +7,7 @@ if ! grep -q "tee_file (dir (mounton" "$WORK_DIR/vendor/etc/selinux/vendor_sepol
     echo "(allow priv_app_202404 tee_file (dir (getattr)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
 fi
 
-SET_PROP "vendor" "ro.vendor.teegris.supported_bootloaders" "$(cat "$(dirname ${BASH_SOURCE[0]})/supported_bootloaders" | tr '\n' ',' | sed 's/,$//')"
+SET_PROP "vendor" "ro.vendor.multitee.supported_bootloaders" "$(cat "$(dirname ${BASH_SOURCE[0]})/supported_bootloaders" | tr '\n' ',' | sed 's/,$//')"
 SET_PROP "vendor" "ro.vendor.multitee.version" "$(cat "$(dirname ${BASH_SOURCE[0]})/module.prop" | grep 'version=' | sed 's/version=//')"
 if git -C "$(dirname ${BASH_SOURCE[0]})" rev-parse --is-inside-work-tree &>/dev/null; then
     SET_PROP "vendor" "ro.vendor.multitee.revision" "$(git -C "$(dirname ${BASH_SOURCE[0]})" rev-parse --short=8 HEAD)"
